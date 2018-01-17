ddca560a71a1ee8431de84e7add9b113663e0684
Pikazza <karanit91@gmail.com>
2018-01-17 09:46:46 +0530

    updating new credentials for paytm

--- paytm/paytm_config.js
+++ paytm/paytm_config.js
@@ -1,7 +1,7 @@
 module.exports = {
   paytm_config: {
 	MID: 'AGGARW60155040679165',
-	WEBSITE: 'www.aggarwaldailyneedsservices.com',
+	WEBSITE: 'APP_STAGING',
     CHANNEL_ID: 'WAP',
     INDUSTRY_TYPE_ID: 'Retail',
     MERCHANT_KEY : 's8rFS8UjSvx!DZWr'
